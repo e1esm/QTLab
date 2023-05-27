333076d9e9510737f4b984dc42c0bccd19a7460d
Egor Mikhailov <egor11022004@gmail.com>
2023-05-27 17:14:21 +0300

    First template of button

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    QMainWindow *mw = new QMainWindow(0, Qt::Window);
+    mw->setWindowTitle("Hello, QT 5");
+    mw->resize(400, 300);
+    mw->show();
+    return app.exec();
 }
